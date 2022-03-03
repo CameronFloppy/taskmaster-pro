26142a5b99e67565259153739825531bce7482bc
Jay Ford <jay.ford45@gmail.com>
2022-03-03 15:16:05 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,7 +135,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -171,16 +171,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    // console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    // console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    // console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    // console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     let tempArr = [];
@@ -218,18 +220,25 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  })
+}, 180000)
 
 function auditTask(taskEl) {
   let date = $(taskEl).find("span").text().trim();
